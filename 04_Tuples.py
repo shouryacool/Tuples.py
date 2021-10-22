aff4c9b5dd779fa056a442200767822d2b72116a
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-10-22 23:13:45 +0530

    Add files via upload

---
+++ 04_Tuples.py
@@ -0,0 +1,16 @@
+# A tuple is an immutable (can’t change or modified) data type in Python.
+
+#Creating a Tuple Using Paranthesis 
+T=(1,2,3,4,5)
+#Empty Tuple
+# T1=()
+T1=(1,)     #This Is A tuple With Single Element
+# T1=(1)    #This Is A Wrong Way To Make a tuple With Single Element
+print(T1)
+#Creating A elements of A tuple 
+# print(T[0])
+
+#Cannot Update a Value of A Tuple 
+# T[0]=34 #Throws An Error 
+
+# Once defined, tuple elements can’t be manipulated or altered
